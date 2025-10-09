de9ee632d26144d30c8ea03230407e075a64158c
Zared Rogers <zared@lightspeedwp.agency>
2025-10-09 07:03:10 +0200

    Updating Single Destination after review

--- patterns/full-width-cta-lion.php
+++ patterns/full-width-cta-lion.php
@@ -17,7 +17,7 @@
  * Notes: Uses assets/img/full-width-cta-image-lion.png.
  */
 ?>
-<!-- wp:cover {"url":"/wp-content/themes/asnz-block-theme/assets/img/full-width-cta-image-lion.png","dimRatio":0,"customOverlayColor":"#ba9d66","isUserOverlayColor":false,"isDark":false,"sizeSlug":"large","metadata":{"name":"Full Width CTA Lion"},"align":"full","style":{"spacing":{"margin":{"top":"0","bottom":"0"}}}} -->
+<!-- wp:cover {"url":"/wp-content/themes/asnz-block-theme/assets/img/full-width-cta-image-lion.png","dimRatio":0,"customOverlayColor":"#ba9d66","isUserOverlayColor":false,"isDark":false,"sizeSlug":"large","metadata":{"name":"Full Width CTA Lion"},"align":"full","style":{"spacing":{"margin":{"top":"0","bottom":"0"}}},"layout":{"type":"constrained"}} -->
 <div class="wp-block-cover alignfull is-light" style="margin-top:0;margin-bottom:0"><img class="wp-block-cover__image-background size-large" alt="" src="/wp-content/themes/asnz-block-theme/assets/img/full-width-cta-image-lion.png" data-object-fit="cover"/><span aria-hidden="true" class="wp-block-cover__background has-background-dim-0 has-background-dim" style="background-color:#ba9d66"></span><div class="wp-block-cover__inner-container"><!-- wp:group {"align":"wide","layout":{"type":"constrained","justifyContent":"right"}} -->
 <div class="wp-block-group alignwide"><!-- wp:group {"className":"is-style-section-full-width-cta","layout":{"type":"constrained"}} -->
 <div class="wp-block-group is-style-section-full-width-cta"><!-- wp:heading -->
